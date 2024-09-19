88073f60c91b6239232b04592cfb1afa30a1bd16
NicolasRDuarte <drnicolasribeiroduarte@gmail.com>
2024-09-19 11:38:38 -0300

    Fazendo a interface do MusicPlayer

--- RaniMusic/screens/MusicPlayer.js
+++ RaniMusic/screens/MusicPlayer.js
@@ -2,19 +2,87 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import {
 	Dimensions,
+	FlatList,
+	Image,
 	SafeAreaView, 
 	StyleSheet,
 	Text,
 	TouchableOpacity,
-	View } from 'react-native'
+	View 
+} from 'react-native'
+import Slider from "@react-native-community/slider";
 import { Ionicons } from "@expo/vector-icons";
+import songs from "../model/data";
 
 const { width, height } = Dimensions.get('window')
 
 const MusicPlayer = () => {
+
+  const renderSongs = ({ item, index }) => {
+    return (
+      <View style={styles.mainImageWrapper}>
+        <View style={[styles.imageWrapper, styles.elevation]}>
+          <Image 
+            source={item.artwork}
+            style={styles.musicImage}
+          />
+        </View>
+      </View>
+    )
+  }
+
   return (
     <SafeAreaView style={styles.container}>
 			<View style={styles.main}>
+
+        <FlatList 
+          renderItem={renderSongs}
+          data={songs}
+          keyExtractor={item => item.id}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          scrollEventThrottle={16}
+          onScroll={() => { }}
+        />
+
+        <View>
+          <Text style={[styles.songContent, styles.songTitle]}>
+            Titulo da Musica
+          </Text>
+          <Text style={[styles.songContent, styles.songArtist]}>
+            Autor da Musica
+          </Text>
+        </View>
+
+        <View>
+          <Slider 
+            style={styles.progressBar}
+            value={10}
+            minimumValue={0}
+            maximumValue={100}
+            thumbTintColor='#FFD369'
+            minimumTrackTintColor='#FFD369'
+            maximumTrackTintColor='#FFF'
+            onSlidingComplete={() => { }}
+          />
+          <View style={styles.progressLevelDuration}>
+            <Text style={styles.progressLabelText}>00:00</Text>
+            <Text style={styles.progressLabelText}>00:00</Text>
+          </View>
+        </View>
+
+        <View style={styles.musicControlsContainer}>
+          <TouchableOpacity onPress={() => { }}>
+            <Ionicons name='play-skip-back-outline' size={35} color="#FFD369" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { }}>
+            <Ionicons name='pause-circle' size={75} color="#FFD369" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name='play-skip-forward-outline' size={35} color="#FFD369" />
+          </TouchableOpacity>
+        </View>
 
 			</View>
 			<View style={styles.footer}>
@@ -62,5 +130,63 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 		justifyContent: 'space-between',
 		width: '80%'
-	}
+	},
+  elevation: {
+    elevation: 5,
+    shadowOffset: {
+      width: 5,
+      height: 5
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84
+  },
+  mainImageWrapper: {
+    width: width,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageWrapper: {
+    width: 340,
+    height: 360,
+    marginVertical: 20,
+  },
+  musicImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 15,
+  },
+  songContent: {
+    textAlign: 'center',
+    color: '#EEEEEE'
+  },
+  songTitle: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  songArtist: {
+    fontSize: 16,
+    fontWeight: '300',
+  },
+  progressBar: {
+    width: 340,
+    height: 40,
+    marginTop: 20,
+    flexDirection: 'row'
+  },
+  progressLevelDuration: {
+    width: 340,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  progressLabelText: {
+    color: '#fff',
+    fontWeight: '500',
+  },
+  musicControlsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '60%',
+    marginTop: 10,
+  }
 })
